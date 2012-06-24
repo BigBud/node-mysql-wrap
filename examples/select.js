f0d939b8e634aa9b0891c40f34110dc56ca1d535
Dmitriyff <dmitriyff@gmail.com>
2012-06-24 16:49:53 +0800

    ADD example for MySQL Select

---
+++ examples/select.js
@@ -0,0 +1,39 @@
+var MySQL = require('../');
+
+	MySQL.connect({
+		"host" : "localhost",
+		"user" : "test",
+		"password" : "test",
+		"database" : "test"
+	});
+
+var user, users;	
+
+//users = (new MySQL()).select('id', 'login').from('users').fetch();
+//console.log(users);
+
+//users = (new MySQL()).from('users').fetch();
+//console.log(users);
+/*
+users = (new MySQL()).select('id', 'login')
+	.from('users')
+	.where({ login: 'ivan', password: '123456'})
+	.fetch();
+
+console.log(users);
+*/
+/*
+users = (new MySQL()).select('id', 'login')
+	.from('users')
+	.where({ login: 'ivan' }, { password: '123456'})
+	.fetch();
+
+console.log(users);
+*/
+user = (new MySQL()).select('id', 'login')
+	.from('users')
+	.where({ login: 'ivan' }, { password: '1213456'})
+	.limit(1)
+	.fetch();
+
+console.log(user);
